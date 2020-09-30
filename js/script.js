18e65864f4e348d514d135f44de9dee1e9f0547b
sergey <sergei.menskii@mail.ru>
2020-09-30 17:56:53 +0300

    first task

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
-console.log(1);
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?');
+
+const personalMovieDB = {    
+	count: numberOfFilms,
+	movies: {},
+	actors: {},
+	genres: [],
+	privat: false
+};
+
+for(let i = 0; i <= 1; i++) {    
+	const film = prompt('Один из последних просмотренных фильмов?');
+	const rating = prompt('На сколько его оцените?');
+    personalMovieDB.movies[film] = rating;
+}
